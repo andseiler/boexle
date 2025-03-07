fa00e4c20b9a16ad7a799259141fc98908364966
Andreas Seiler <mail@andreas-seiler.net>
2025-03-07 18:47:43 +0100

    interim

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="bg-tertiary-200" id="home">
     <!-- Header bleibt unverändert -->
-    <header class="bg-primarycontrast-500 sticky z-50 transition-all duration-300" :class="{'shadow-xl': isScrolled, '-top-full': !showHeader, 'top-0': showHeader}">
+    <header class="bg-primarycontrast-500 sticky z-50 transition-all duration-300"
+            :class="{'shadow-xl': isScrolled, '-top-full': !showHeader, 'top-0': showHeader}">
       <div class="container mx-auto px-4 py-2 flex items-center justify-between max-w-screen-lg">
-        <div class="text-2xl font-bold text-primary-500 flex flex-col justify-center cursor-pointer" @click.prevent="scrollToSection('home')">
-<!--          <img class="h-20" src="/images/pocketledge-logo-v2-green.svg" alt="">-->
+        <div class="text-2xl font-bold text-primary-500 flex flex-col justify-center cursor-pointer"
+             @click.prevent="scrollToSection('home')">
+          <!--          <img class="h-20" src="/images/pocketledge-logo-v2-green.svg" alt="">-->
           <span class="gloria-hallelujah-regular py-4">POCKETLEDGE</span>
         </div>
         <nav class="flex gap-6">
-          <a @click.prevent="scrollToSection('price')" class="cursor-pointer text-gray-50 hover:text-primary-500 flex gap-1"><currency-euro-icon class="w-6"></currency-euro-icon><span>Preise</span></a>
-          <a @click.prevent="scrollToSection('faq')" class="cursor-pointer text-gray-50 hover:text-primary-500 flex gap-1"><question-mark-circle-icon class="w-6"></question-mark-circle-icon><span>FAQ</span></a>
-          <a @click="showContactModal = true" class="cursor-pointer text-gray-50 hover:text-primary-500 flex gap-1">
-            <chat-bubble-oval-left-ellipsis-icon class="w-6"></chat-bubble-oval-left-ellipsis-icon><span>Kontakt</span></a>
+          <a @click.prevent="scrollToSection('price')"
+             class="cursor-pointer text-gray-50 hover:text-primary-500 hover:border-primary-500 border-2 border-gray-50 p-1 rounded-xl flex gap-1 ">
+            <currency-euro-icon class="w-6"></currency-euro-icon>
+            <span class="hidden sm:inline">Preise</span>
+          </a>
+          <a @click.prevent="scrollToSection('faq')"
+             class="cursor-pointer text-gray-50 hover:text-primary-500 flex gap-1 hover:border-primary-500 border-2 border-gray-50 p-1 rounded-xl">
+            <question-mark-circle-icon class="w-6"></question-mark-circle-icon>
+            <span class="hidden sm:inline">FAQ</span></a>
+          <a @click="showContactModal = true" class="cursor-pointer text-gray-50 hover:text-primary-500 flex gap-1 hover:border-primary-500 border-2 border-gray-50 p-1 rounded-xl">
+            <chat-bubble-oval-left-ellipsis-icon class="w-6"></chat-bubble-oval-left-ellipsis-icon>
+            <span class="hidden sm:inline">Kontakt</span></a>
           <ModalComponent :isVisible="showContactModal" @close="showContactModal = false" title="Kontakt">
             <ContactForm class="w-80"></ContactForm>
           </ModalComponent>
@@ -19,7 +29,7 @@
       </div>
     </header>
     <!-- Hero-Bereich als Bilder-Slider mit zentralem Overlay -->
-    <section  class="relative bg-linear-to-b from-tertiary-500 to-tertiary-200 ">
+    <section class="relative bg-linear-to-b from-tertiary-500 to-tertiary-200 ">
       <Swiper
           :slides-per-view="1"
           :loop="true"
@@ -27,14 +37,14 @@
           class="h-[700px]"
       >
         <SwiperSlide>
-          <img src="/images/bg.png" alt="Slide 1" class="object-cover w-full h-full" />
+          <img src="/images/bg.png" alt="Slide 1" class="object-cover w-full h-full"/>
         </SwiperSlide>
-<!--        <SwiperSlide>-->
-<!--          <img src="/images/fs-blunt-art.jpg" alt="Slide 2" class="object-cover w-full h-full" />-->
-<!--        </SwiperSlide>-->
-<!--        <SwiperSlide>-->
-<!--          <img src="/images/bs-crooks-art.jpg" alt="Slide 3" class="object-cover w-full h-full" />-->
-<!--        </SwiperSlide>-->
+        <!--        <SwiperSlide>-->
+        <!--          <img src="/images/fs-blunt-art.jpg" alt="Slide 2" class="object-cover w-full h-full" />-->
+        <!--        </SwiperSlide>-->
+        <!--        <SwiperSlide>-->
+        <!--          <img src="/images/bs-crooks-art.jpg" alt="Slide 3" class="object-cover w-full h-full" />-->
+        <!--        </SwiperSlide>-->
         <div class="swiper-pagination"></div>
         <div class="absolute inset-0 flex flex-col items-center justify-center bg-black opacity-0 px-4 z-40">
 
@@ -42,17 +52,20 @@
       </Swiper>
       <div class=" absolute inset-0 flex flex-col items-center justify-center px-4 z-20 ">
         <h1 class="fade-in text-4xl md:text-6xl font-extrabold mb-4 text-gray-50">
-<!--          <img src="/images/pocketledge-logo-v2-white.svg" alt="">-->
+          <!--          <img src="/images/pocketledge-logo-v2-white.svg" alt="">-->
           <span class="gloria-hallelujah-regular custom-text-shadow">POCKETLEDGE</span>
         </h1>
         <p class="text-lg md:text-2xl mb-8 text-gray-50 text-center custom-text-shadow">
           Extrem stabil, in 1 Minute auf- und abgebaut, <br>passt perfekt in eine Eurobox (60x40x30 cm).
         </p>
         <div @click="showContactModal = true" class="gradient-button mb-8">
-          <shopping-cart-icon class="w-6"></shopping-cart-icon><span>Jetzt bestellen</span>
+          <shopping-cart-icon class="w-6"></shopping-cart-icon>
+          <span>Jetzt bestellen</span>
         </div>
-        <div @click="showModal = true" class=" w-fit gradient-button custom-color from-primarycontrast-500 to-primarycontrast-600 ">
-          <PlayPauseIcon class="w-6"></PlayPauseIcon><span>Aufbau in Aktion ansehen</span>
+        <div @click="showModal = true"
+             class=" w-fit gradient-button custom-color from-primarycontrast-500 to-primarycontrast-600 ">
+          <PlayPauseIcon class="w-6"></PlayPauseIcon>
+          <span>Aufbau in Aktion ansehen</span>
         </div>
       </div>
     </section>
@@ -92,7 +105,8 @@
         </div>
         <div class="flex justify-center mt-12">
           <div @click="showModal = true" class=" w-fit gradient-button gradient-button-outline ">
-            <PlayPauseIcon class="w-6"></PlayPauseIcon><span>Aufbau in Aktion ansehen</span>
+            <PlayPauseIcon class="w-6"></PlayPauseIcon>
+            <span>Aufbau in Aktion ansehen</span>
           </div>
           <ModalComponent :isVisible="showModal" @close="showModal = false" title="Aufbau">
             <iframe
@@ -113,17 +127,20 @@
         <div class="grid md:grid-cols-3 gap-8">
 
           <div class="bg-white rounded-lg shadow p-6 text-center">
-            <p class="text-gray-700 italic">"PocketLedge ist ein Gamechanger! Super stabil und perfekt für jede Session!"</p>
+            <p class="text-gray-700 italic">"PocketLedge ist ein Gamechanger! Super stabil und perfekt für jede
+              Session!"</p>
             <p class="mt-4 font-bold text-primary-500">- Max aus Berlin</p>
           </div>
 
           <div class="bg-white rounded-lg shadow p-6 text-center">
-            <p class="text-gray-700 italic">"Ich liebe meine Pocketledge, so schnell aufgebaut und passt locker in mein Lastenrad."</p>
+            <p class="text-gray-700 italic">"Ich liebe meine Pocketledge, so schnell aufgebaut und passt locker in mein
+              Lastenrad."</p>
             <p class="mt-4 font-bold text-primary-500">- Lisa aus Wien</p>
           </div>
 
           <div class="bg-white rounded-lg shadow p-6 text-center">
-            <p class="text-gray-700 italic">"Der Landi isch volle bärig, aber gibt halt keine Ledge. Jetzt bring ich einfach meine Eigene mit"</p>
+            <p class="text-gray-700 italic">"Der Landi isch volle bärig, aber gibt halt keine Ledge. Jetzt bring ich
+              einfach meine Eigene mit"</p>
             <p class="mt-4 font-bold text-primary-500">- Chris aus Innsbruck</p>
           </div>
 
@@ -139,29 +156,36 @@
         </p>
 
         <!-- Price Card -->
-        <div class="max-w-lg mx-auto bg-tertiary-200 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center">
+        <div
+            class="max-w-lg mx-auto bg-tertiary-200 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center">
           <h3 class="ont-bold text-primary-500 mb-4 gloria-hallelujah-regular text-4xl">POCKETLEDGE</h3>
           <p class="text-4xl font-extrabold text-gray-900 mb-6">280 €</p>
           <p class="text-gray-700 mb-6">Handgefertigt in Österreich – sofort einsatzbereit!</p>
 
           <ul class="text-gray-700 text-left space-y-3 mb-6">
             <li class="flex items-center">
-              <CheckBadgeIcon class="w-6 text-primary-500"></CheckBadgeIcon> <span class="ml-2">Hochwertige Handarbeit – Made in Austria 🇦🇹</span>
+              <CheckBadgeIcon class="w-6 text-primary-500"></CheckBadgeIcon>
+              <span class="ml-2">Hochwertige Handarbeit – Made in Austria 🇦🇹</span>
             </li>
             <li class="flex items-center">
-              <CheckBadgeIcon class="w-6 text-primary-500"></CheckBadgeIcon> <span class="ml-2">Modulares Design – passt in jede Eurobox (60x40x30)</span>
+              <CheckBadgeIcon class="w-6 text-primary-500"></CheckBadgeIcon>
+              <span class="ml-2">Modulares Design – passt in jede Eurobox (60x40x30)</span>
             </li>
             <li class="flex items-center">
-              <CheckBadgeIcon class="w-6 text-primary-500"></CheckBadgeIcon> <span class="ml-2">Sekundenschneller Aufbau – in 1 Minute bereit</span>
+              <CheckBadgeIcon class="w-6 text-primary-500"></CheckBadgeIcon>
+              <span class="ml-2">Sekundenschneller Aufbau – in 1 Minute bereit</span>
             </li>
             <li class="flex items-center">
-              <CheckBadgeIcon class="w-6 text-primary-500"></CheckBadgeIcon> <span class="ml-2">Stabile Metallkonstruktion – kein Wackeln, kein Verrutschen</span>
+              <CheckBadgeIcon class="w-6 text-primary-500"></CheckBadgeIcon>
+              <span class="ml-2">Stabile Metallkonstruktion – kein Wackeln, kein Verrutschen</span>
             </li>
           </ul>
 
           <!-- Order Button -->
-          <div @click="showContactModal=true" class="gradient-button custom-color from-primarycontrast-500 to-primarycontrast-600">
-            <shopping-cart-icon class="w-6"></shopping-cart-icon>Jetzt bestellen
+          <div @click="showContactModal=true"
+               class="gradient-button custom-color from-primarycontrast-500 to-primarycontrast-600">
+            <shopping-cart-icon class="w-6"></shopping-cart-icon>
+            Jetzt bestellen
           </div>
         </div>
 
@@ -180,7 +204,6 @@
     </div>
 
 
-
     <div class="bg-tertiary-200">
       <section id="faq" class="container mx-auto px-4 py-16 max-w-screen-xl">
         <h2 class="text-center text-primary-500 text-2xl font-bold mb-8">Häufig gestellte Fragen (FAQ)</h2>
@@ -189,17 +212,20 @@
 
           <div class="bg-white rounded-lg shadow p-6">
             <h3 class="font-bold text-primary-500">Wie lange dauert der Aufbau wirklich?</h3>
-            <p class="text-gray-700 mt-2">Der Aufbau dauert tatsächlich nur 1 Minute! Dank unseres cleveren Designs kannst du PocketLedge blitzschnell einsatzbereit machen.</p>
+            <p class="text-gray-700 mt-2">Der Aufbau dauert tatsächlich nur 1 Minute! Dank unseres cleveren Designs
+              kannst du PocketLedge blitzschnell einsatzbereit machen.</p>
           </div>
 
           <div class="bg-white rounded-lg shadow p-6">
             <h3 class="font-bold text-primary-500">Wie stabil ist PocketLedge?</h3>
-            <p class="text-gray-700 mt-2">PocketLedge besteht aus hochwertigem Stahl und bleibt stabil, egal ob du Street- oder Park-Tricks machst.</p>
+            <p class="text-gray-700 mt-2">PocketLedge besteht aus hochwertigem Stahl und bleibt stabil, egal ob du
+              Street- oder Park-Tricks machst.</p>
           </div>
 
           <div class="bg-white rounded-lg shadow p-6">
             <h3 class="font-bold text-primary-500">Passt PocketLedge wirklich in eine Eurobox?</h3>
-            <p class="text-gray-700 mt-2">Ja! Das Design ist so optimiert, dass PocketLedge perfekt in eine Eurobox (60x40x30 cm) passt.</p>
+            <p class="text-gray-700 mt-2">Ja! Das Design ist so optimiert, dass PocketLedge perfekt in eine Eurobox
+              (60x40x30 cm) passt.</p>
           </div>
 
         </div>
@@ -250,8 +276,10 @@
         </div>
         <div class="mt-8 flex flex-col items-center justify-center gap-6">
           <h3 class="text-lg font-bold text-primary-500 mb-2">Willst du auch in unserer Galerie erscheinen?</h3>
-          <div @click="showContactModal=true" class="gradient-button custom-color from-primarycontrast-500 to-primarycontrast-600">
-            <photo-icon class="w-6"></photo-icon>Schick uns dein Bild!
+          <div @click="showContactModal=true"
+               class="gradient-button custom-color from-primarycontrast-500 to-primarycontrast-600">
+            <photo-icon class="w-6"></photo-icon>
+            Schick uns dein Bild!
           </div>
         </div>
       </section>
@@ -273,16 +301,23 @@
 </template>
 
 <script setup lang="ts">
-import { Swiper, SwiperSlide } from 'swiper/vue';
+import {Swiper, SwiperSlide} from 'swiper/vue';
 //@ts-ignore
 import 'swiper/css';
 //@ts-ignore
 import 'swiper/css/pagination';
 import SwiperCore from 'swiper';
-import { Autoplay, Pagination } from 'swiper/modules';
+import {Autoplay, Pagination} from 'swiper/modules';
 import {onMounted, onUnmounted, ref} from "vue";
-import { PlayPauseIcon, ShoppingCartIcon, CurrencyEuroIcon, QuestionMarkCircleIcon, ChatBubbleOvalLeftEllipsisIcon, PhotoIcon} from '@heroicons/vue/24/outline'
-import { CheckBadgeIcon} from '@heroicons/vue/24/solid'
+import {
+  PlayPauseIcon,
+  ShoppingCartIcon,
+  CurrencyEuroIcon,
+  QuestionMarkCircleIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  PhotoIcon
+} from '@heroicons/vue/24/outline'
+import {CheckBadgeIcon} from '@heroicons/vue/24/solid'
 import ModalComponent from "../components/ModalComponent.vue";
 import ContactForm from "../components/ContactForm.vue"
 
@@ -300,7 +335,7 @@ const scrollToSection = (id: string) => {
   console.log('scroll')
   const element = document.getElementById(id);
   if (element) {
-    element.scrollIntoView({ behavior: "smooth" });
+    element.scrollIntoView({behavior: "smooth"});
 
     //@ts-ignore
     history.pushState(null, null, " ");
@@ -320,10 +355,16 @@ onMounted(() => window.addEventListener("scroll", handleScroll));
 onUnmounted(() => window.removeEventListener("scroll", handleScroll));
 </script>
 
-<style lang="scss" >
+<style lang="scss">
 @keyframes fadeIn {
-  from { opacity: 0; transform: translateY(20px); }
-  to { opacity: 1; transform: translateY(0); }
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 }
 
 .fade-in {
@@ -331,15 +372,15 @@ onUnmounted(() => window.removeEventListener("scroll", handleScroll));
 }
 
 
-
 .custom-text {
   font-family: 'GloriaHallelujah', sans-serif;
 }
-.custom-text-shadow{
+
+.custom-text-shadow {
   text-shadow: 2px 2px 3px #2b5544;
 }
 
-.custom-box-shadow{
+.custom-box-shadow {
   box-shadow: 2px 2px 3px #2b5544;
 }
 
