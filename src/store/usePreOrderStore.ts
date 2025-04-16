6d9022efe36459881002ee44f49f7f2bb37e79e0
Andreas Seiler <mail@andreas-seiler.net>
2025-04-16 20:01:56 +0200

    gtag

--- src/store/usePreOrderStore.ts
+++ src/store/usePreOrderStore.ts
@@ -1,6 +1,6 @@
 import { computed, ref } from "vue";
 
-const preOrderCountRef = ref(17);
+const preOrderCountRef = ref(21);
 const totalAvailableRef = ref(30);
 const rebateRef = ref(50);
 const priceRef = ref(320);
